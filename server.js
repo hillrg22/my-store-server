d28bff0cccf3483b05909bd7993ad673a7aabd61
Rob Hill <hillrg22@gmail.com>
2019-06-27 15:24:42 -0600

    installed eslint

--- server.js
+++ server.js
@@ -19,14 +19,26 @@ app.get('/api/test', (req, res, next)=> {
   res.json({
     message: 'Route working'
   })
+  
+  // const error = new Error('it blew up')
+  // next(error)
 })
 
 // Error handling
 
+app.use(notFound)
+app.use(errorHandler)
 
+function notFound(req, res, next) {
+  res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+}
 
-
-
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}
 
 
 
