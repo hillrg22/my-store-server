9f393914f7703bf08228a276e22d4b9b330d192b
Rob Hill <hillrg22@gmail.com>
2019-06-27 15:04:09 -0600

    basic get route

--- server.js
+++ server.js
@@ -6,7 +6,22 @@ const PORT = process.env.PORT || 4000
 const app = express()
 
 
+// Middleware
+app.use(cors())
+  // morgan provides logging info to console
+app.use(morgan('dev'))
 
+// Router files
+
+
+// Routes
+app.get('/api/test', (req, res, next)=> {
+  res.json({
+    message: 'Route working'
+  })
+})
+
+// Error handling
 
 
 
