5ab0e65a362a24063810323a5401d4105dcf9e61
Rob Hill <hillrg22@gmail.com>
2019-07-18 14:59:34 -0600

    checkJwt protecting select routes/requests

--- server.js
+++ server.js
@@ -2,14 +2,38 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 
 const PORT = process.env.PORT || 4000
 const app = express()
 
+// Set up Auth0 configuration
+const authConfig = {
+  domain: 'dev-0sd3i2ce.auth0.com',
+  audience: 'http://localhost:4000'
+}
+
+// Define middleware that validates incoming bearer tokens
+// using JWKS from dev-0sd3i2ce.auth0.com
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ['RS256']
+})
+
 
 // Middleware
+
 app.use(cors())
 // morgan provides logging info to console
 app.use(morgan('dev'))
@@ -94,6 +118,13 @@ app.post('/api/checkout', async (req, res, next) => {
   catch (error) {
     next(error)
   }
+})
+
+// Test endpoint that must be called with an access token
+app.get('/api/external', checkJwt, (req, res) => {
+  res.json({
+    msg: 'Your Access Token was successfully validated!'
+  })
 })
 
 
